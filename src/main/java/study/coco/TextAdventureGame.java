572d9cfd8730f0aaa5fe1739235b545c27323482
king <kjm1491@naver.com>
2024-03-03 15:56:11 +0900

    1

--- src/main/java/study/coco/TextAdventureGame.java
+++ src/main/java/study/coco/TextAdventureGame.java
@@ -1,2 +1,26 @@
-package study.coco;public class TextAdventureGame {
+package study.coco;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TextAdventureGame {
+    public static void main(String[] args) {
+        System.out.println("Starting the housework!");
+
+        Player player = new Player();
+        Map<String, Room> rooms = createRooms();
+        player.setCurrentRoom(rooms.get("Living Room"));
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            }
+        }
+        scanner.close();
+    }
+
+    private static Map<String, Room> createRooms() {
+        Map<String, Room> rooms = new HashMap<>();
+
+    }
 }
